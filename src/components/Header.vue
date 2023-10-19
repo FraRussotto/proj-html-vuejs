5f3000ff6ec2cf03a6e07d4d3399cac240705bc0
FraRussotto <russottofrancesco1@gmail.com>
2023-10-19 12:24:01 +0200

    [edit] header_top in header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,9 +5,45 @@
 </script>
 
 <template>
-  <h1>Header</h1>
+
+
+  <!-- Suddivisione dell'header in 2 contenitori separati -->
+    <div class="header_top">
+      <div class="fr_container d-flex justify-content-between">
+        <div class="left d-flex">
+          <div class="location"> Palermo, Italia</div>
+          <div class="email">demo@example.com</div>
+        </div>
+        <div class="right d-flex">
+          <div class="hours_active">8.30AM-8.30PM</div>
+          <div class="social">
+            <ul class="d-flex list-unstyled ">
+              <li>social</li>
+              <li>social</li>
+              <li>social</li>
+              <li>social</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="header_bottom">
+
+    </div>
+
 </template>
 
-<style>
+<style lang="scss" scoped>
 
+@use '../scss/variables' as *;
+  .header_top{
+    height: 30px;
+    color: white;
+    background-color: $green;
+    .fr_container{
+      width: $container_max;
+      margin: 0 auto;
+    }
+  }
 </style>
